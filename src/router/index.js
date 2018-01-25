bfee7e40d478edd7dfcc9255dee37acb3372f514
shicongrui <1102163949@qq.com>
2018-01-25 19:58:01 +0800

    添加路由详情页

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 const index = () => import('@/pages/index')
+const details = () => import('@/pages/details')
 
 Vue.use(Router)
 
@@ -11,6 +12,11 @@ export default new Router({
       path: '/',
       name: 'index',
       component: index
+    },
+    {
+      path: 'details',
+      name: 'details',
+      component: details
     }
   ]
 })
