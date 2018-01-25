51b88488cf7f0b283c2cc0561be3f018f4e73674
shicongrui <1102163949@qq.com>
2018-01-25 19:58:16 +0800

    详情页面的建立

---
+++ src/pages/details.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="detail">
+        <h1>详情</h1>
+    </div>
+</template>
+<script>
+export default {
+  name: 'detail',
+  data () {
+    return {}
+  },
+  mounted () {
+    console.log(this.$route.params)
+  }
+}
+</script>
+<style>
+
+</style>
