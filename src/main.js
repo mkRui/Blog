6c385c7b8886882a648ac7b0091c348d31f04dc2
shicongrui <1102163949@qq.com>
2018-01-25 19:59:12 +0800

    挂载 element-ui组件

--- src/main.js
+++ src/main.js
@@ -5,7 +5,11 @@ import App from './layout/App.vue'
 import router from './router'
 import store from './store/index'
 import './assets/scss/index.scss'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 Vue.config.productionTip = false
+
+Vue.use(ElementUI)
 
 /* eslint-disable no-new */
 new Vue({
