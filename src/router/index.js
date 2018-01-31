393be345e357aafc6d92dd4997700cd903f3075d
shicongrui <1102163949@qq.com>
2018-01-31 13:10:19 +0800

    增加路由进度条效果

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,36 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 
 const index = () => import('@/pages/index')
 const details = () => import('@/pages/details')
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
+    { path: '/', redirect: '/index' },
     {
-      path: '/',
+      path: '/index',
       name: 'index',
       component: index
     },
     {
-      path: 'details',
+      path: '/details',
       name: 'details',
       component: details
     }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  NProgress.start()
+  next()
+})
+
+router.afterEach(transition => {
+  NProgress.done()
+})
+
+export default router
