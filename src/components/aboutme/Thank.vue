53c0c188dd4a1b6d8caf7ad8b7b5216f7f2c1df3
shicongrui <1102163949@qq.com>
2018-02-15 08:47:01 +0800

    关于作者页面的部分组件

---
+++ src/components/aboutme/Thank.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="tanks clearfix">
+    <h3>{{ title }}</h3>
+    <ul>
+      <li class="clearfix" v-for="(item, index) in hot" :key='index'>
+        <div class="font">
+          <span>{{ item.title }}</span>
+          <span>----------</span>
+          <span>{{ item.min }}</span>
+        </div>
+      </li>
+    </ul>
+    <div class="address">
+      <router-link to="/index">回到主页</router-link>
+      <span>···</span>
+      <router-link to="/index">留言墙</router-link>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'tanks',
+  data () {
+    return {
+      title: '最新链接',
+      hot: [
+        {title: 'web前端学习之路', min: 'js html css', image: true, url: 'dasdasdasdad', id: '42345t5'},
+        {title: '开始了 现在', min: 'js hl css', image: false, id: '3242345'},
+        {title: 'nodeJS后台', min: 'js html css', image: true, url: 'dasdasdasdad', id: '32445t5'},
+        {title: 'java8开始', min: 'js html css', image: false, id: '32425t5'},
+        {title: '开始了 现在', min: 'js hl css', image: false, id: '3242345'},
+        {title: 'nodeJS后台', min: 'js html css', image: true, url: 'dasdasdasdad', id: '32445t5'},
+        {title: 'java8开始', min: 'js html css', image: false, id: '32425t5'}
+      ]
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.tanks {
+  float: right;
+  width: 345px;
+  height: 345px;
+  border-left: 1px solid $border;
+  margin-top: 10px;
+  box-sizing: border-box;
+  padding:10px;
+  padding-left: 20px;
+  h3 {
+    font-weight: normal;
+  }
+  ul {
+    width: 100%;
+    li {
+      box-sizing: border-box;
+      padding: 7px;
+      .font {
+        float: left;
+        line-height:2;
+      }
+    }
+  }
+  .address {
+    margin-left: 5px;
+    a {
+      font-size: 13px;
+    }
+  }
+}
+</style>
