cf757aa4d5a53c00d49efbd7ad11dcc8e1a2072c
shicongrui <1102163949@qq.com>
2018-02-26 15:07:00 +0800

    全局定义过滤器的方法

--- src/main.js
+++ src/main.js
@@ -10,12 +10,18 @@ import 'element-ui/lib/theme-chalk/index.css'
 import VueSimplemde from 'vue-simplemde'
 import 'simplemde/dist/simplemde.min.css'
 import 'highlight.js/styles/github.css'
+import * as filters from './filter'
 
 Vue.config.productionTip = false
 
 Vue.use(VueSimplemde)
 
 Vue.use(ElementUI)
+
+// 全局实例化过滤器
+Object.keys(filters).forEach((item) => {
+  Vue.filter(item, filters[item])
+})
 
 /* eslint-disable no-new */
 new Vue({
