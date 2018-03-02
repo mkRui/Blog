a931dd06a8a461bfd776860f0df5ffdde497dfff
shicongrui <1102163949@qq.com>
2018-03-02 08:23:20 +0800

    留言墙功能基本实现完毕

--- src/components/leave/waterfall.vue
+++ src/components/leave/waterfall.vue
@@ -2,12 +2,17 @@
   <div class="leave">
     <div class="leaveMain">
       <ul v-for="(item, index) in message" :key="index" ref="ul">
-        <li v-for="(elem, info) in item" :key="info">{{ elem }}</li>
+        <li v-for="(elem, info) in item" :key="info">
+          <h5 class="date">{{elem.date}}</h5>
+          <contents :contents="elem.content | emojiContent"></contents>
+          <h5 class="right">{{elem.name ? elem.name : '好懒呐！'}}</h5>
+        </li>
       </ul>
     </div>
   </div>
 </template>
 <script>
+import contents from './../../components/article/content'
 export default {
   name: 'leave',
   data () {
@@ -21,7 +26,21 @@ export default {
       compare: [],
       num: 0,
       index: 0,
-      info: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+      info: []
+    }
+  },
+  components: {
+    contents
+  },
+  props: ['msgConten'],
+  watch: {
+    async msgConten (data) {
+      this.info.push(data)
+      for (var i = 0; i < this.info.length; i++) {
+        await this.calc()
+        await this.message[this.index].push(this.info[i])
+      }
+      this.info = []
     }
   },
   methods: {
@@ -40,6 +59,7 @@ export default {
     }
   },
   async mounted () {
+    console.log(this.msgConten)
     // 定时器的写法
     // var index = 0
     // var timer = null
@@ -64,12 +84,31 @@ export default {
       ul {
         width: 250px;
         float: left;
-        background: #444;
         margin-left: 25px;
         margin-right: 25px;
         li {
+          position: relative;
+          background: #ccc;
           width: 100%;
-          min-height: 40px;
+          min-height: 100px;
+          margin-bottom: 10px;
+          box-sizing: border-box;
+          padding: 10px;
+          padding-bottom: 20px;
+          word-wrap:break-word;
+          border-radius: 4px;
+          h5 {
+            font-size: 15px;
+            font-weight: normal;
+          }
+          .date {
+            margin-bottom: 10px;
+          }
+          .right {
+            position: absolute;
+            right: 5px;
+            bottom: 5px;
+          }
         }
       }
     }
