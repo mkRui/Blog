6ed5ba6cf1a3f3b4b27749d620c45bd2a7af59a5
shicongrui <1102163949@qq.com>
2018-03-02 08:24:46 +0800

    路由列表进行样式更改

--- src/components/sidebar/linkList.vue
+++ src/components/sidebar/linkList.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="linkList">
+  <div class="linkList" v-fixed>
       <div class='thislink'><router-link to="/aboutme">关于作者</router-link></div>
       <div>···</div>
       <div class='thislink'><router-link to="/message">留言墙</router-link></div>
@@ -9,6 +9,19 @@
 export default {
   name: 'linkList',
   mounted () {
+  },
+  directives: {
+    fixed: {
+      inserted: function (el) {
+        window.addEventListener('scroll', () => {
+          if (window.scrollY > 840) {
+            el.classList.add('fixeds')
+          } else {
+            el.classList.remove('fixeds')
+          }
+        })
+      }
+    }
   }
 }
 </script>
@@ -23,5 +36,13 @@ export default {
       margin-left: 0px;
       margin-right: 0px;
     }
+  }
+  .fixeds {
+    width: 346.55px;
+    max-width: 346.55px;
+    position: fixed;
+    top: 407px;
+    transform: translateY(10px);
+    transition: .35s;
   }
 </style>
