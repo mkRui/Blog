8ce3273e63e834f62dd6d9f79bb44a0814a11097
shicongrui <1102163949@qq.com>
2018-03-05 17:00:22 +0800

    axios 添加拦截器

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,9 +1,23 @@
 import axios from 'axios'
 import * as config from '../config.js'
+import queryString from 'queryString'
 
 const ax = axios.create({
   baseURL: config.address,
   timeout: 5000
+})
+
+axios.interceptors.request.use(config => {
+  if (config.method === 'post' ||
+      config.method === 'put' ||
+      config.method === 'delete' ||
+      config.method === 'patch'
+  ) {
+    config.data = queryString.stringify(config.data)
+  }
+  return config
+}, error => {
+  return Promise.reject(error)
 })
 
 export default ax
