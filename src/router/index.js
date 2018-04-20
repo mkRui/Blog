054e10d87f7bf257dfa623813f54cea33af7fc5e
shicongrui <1102163949@qq.com>
2018-04-20 08:15:03 +0800

    路由懒加载

--- src/router/index.js
+++ src/router/index.js
@@ -3,10 +3,23 @@ import Router from 'vue-router'
 import NProgress from 'nprogress'
 import 'nprogress/nprogress.css'
 
-const index = () => import('@/pages/index')
-const details = () => import('@/pages/details')
-const aboutMe = () => import('@/pages/aboutme')
-const message = () => import('@/pages/leavmessage')
+// 路由容器
+const home = r => require.ensure([], () => r(require('./../pages/home.vue')), 'group1')
+
+// 首页
+const index = r => require.ensure([], () => r(require('./../pages/index/index.vue')), 'group2')
+
+// 详情
+const details = r => require.ensure([], () => r(require('./../pages/details/index.vue')), 'group3')
+
+// 关于我
+const aboutMe = r => require.ensure([], () => r(require('./../pages/about/index.vue')), 'group4')
+
+// 留言墙
+const message = r => require.ensure([], () => r(require('./../pages/message/index.vue')), 'group5')
+
+// 自定义定制
+const custom = r => require.ensure([], () => r(require('./../pages/custom.vue')), 'group6')
 
 Vue.use(Router)
 
@@ -14,24 +27,44 @@ const router = new Router({
   routes: [
     { path: '/', redirect: '/index' },
     {
-      path: '/index',
+      path: '/',
       name: 'index',
-      component: index
+      component: home,
+      children: [
+        {path: '/index', component: index, name: '首页列表', meta: {page: 'index'}}
+      ]
     },
     {
-      path: '/details',
+      path: '/',
       name: 'details',
-      component: details
+      component: home,
+      children: [
+        {path: '/details', component: details, name: '列表详情', meta: {page: 'details'}}
+      ]
     },
     {
-      path: '/aboutMe',
+      path: '/',
       name: 'aboutMe',
-      component: aboutMe
+      component: home,
+      children: [
+        {path: '/aboutMe', component: aboutMe, name: '关于作者', meta: {page: 'aboutMe'}}
+      ]
     },
     {
-      path: '/message',
+      path: '/',
       name: 'message',
-      component: message
+      component: home,
+      children: [
+        {path: '/message', component: message, name: '留言墙', meta: {page: 'message'}}
+      ]
+    },
+    {
+      path: '/',
+      name: 'custom',
+      component: home,
+      children: [
+        {path: '/custom', component: custom, name: '定制', meta: {page: 'custom'}}
+      ]
     }
   ]
 })
