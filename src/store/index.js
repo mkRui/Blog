28f173d9975678738e204edb50f411756b0c8948
shicongrui <1102163949@qq.com>
2018-04-22 11:18:37 +0800

    vuex 增加喜欢判断

--- src/store/index.js
+++ src/store/index.js
@@ -6,25 +6,31 @@ import article from './module/article'
 Vue.use(Vuex)
 
 const state = {
-
+  ifDetail: false
 }
 
 const getters = {
-
+  ifDetail (state) {
+    return state.ifDetail
+  }
 }
 
 const actions = {
-
+  detailLike ({commit}, data) {
+    commit('DETAILLIKE', data)
+  }
 }
 
 const mutations = {
-
+  DETAILLIKE (state, data) {
+    state.ifDetail = data
+  }
 }
 
 export default new Vuex.Store({
   state,
-  getters,
   actions,
+  getters,
   mutations,
   modules: {
     article
